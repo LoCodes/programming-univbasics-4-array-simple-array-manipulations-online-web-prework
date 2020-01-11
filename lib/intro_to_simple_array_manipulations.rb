3209a1476f93ee2420042898203a7be3ca1436c5
Loren Cipriano <loren25cipriano@gmail.com>
2020-01-11 06:44:04 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,43 @@
+require "pry"
+
+
+def using_push(colors_in_the_rainbow, next_color)
+   using_push = ["red", "orange", "yellow", "green", "blue", "indigo"]
+   using_push.push(next_color)
+ end
+ 
+def using_unshift(bouroughs_in_nyc, new_neighborhood)
+  using_unshift = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  using_unshift.unshift(new_neighborhood)
+end
+  
+#def using_pop(continents)
+#  using_pop.pop
+#end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array, more_favs)
+  array.concat(more_favs)
+end
+
+def using_insert(array, element)
+  new_array = using_insert(list_of_programming_languages, another_language)
+  array.insert
+end
+  
+
